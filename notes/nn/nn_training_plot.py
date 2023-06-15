93715529fe1838ea75cd488eb616a412f8322fc7
Kaewin <gerbald@protonmail.com>
2023-06-14 22:48:10 -0500

    new work on notes

---
+++ notes/nn/nn_training_plot.py
@@ -0,0 +1,19 @@
+def visualize_training_results(results):
+    history = results.history
+    plt.figure()
+    plt.plot(history['val_loss'])
+    plt.plot(history['loss'])
+    plt.legend(['val_loss', 'loss'])
+    plt.title('Loss')
+    plt.xlabel('Epochs')
+    plt.ylabel('Loss')
+    plt.show()
+    
+    plt.figure()
+    plt.plot(history['val_acc'])
+    plt.plot(history['acc'])
+    plt.legend(['val_acc', 'acc'])
+    plt.title('Accuracy')
+    plt.xlabel('Epochs')
+    plt.ylabel('Accuracy')
+    plt.show()
